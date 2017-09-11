2fd999b2c23b187a71f53e4017dd8f1ac290ae59
Anthony Hennessey <ah@ravingmantis.com>
2017-09-11 11:53:17 +0100

    Modify order of cleaning steps.

--- clean.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -e
-
-# all unix line endings
-dos2unix *.txt
-# convert to 7-bit ASCII
-perl -C -MText::Unidecode -n -i -e'print unidecode($_)' *.txt
-# remove all underscores
-perl -pi -e 's/_//g' *.txt
-
