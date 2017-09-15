115aeddbadbb4ce05324b99597b3f24a89e53054
Anthony Hennessey <ah@ravingmantis.com>
2017-09-15 19:40:49 +0100

    Added corpora.json metadata file. Reorganized documentation files. Updated documentation.X:

---
+++ scripts/bib2json.R
@@ -0,0 +1,23 @@
+library(jsonlite)
+library(RefManageR)
+
+corpora <- data.frame(
+    "id" = c("ChiLit"),
+    "title" = c("Children's Literature"),
+    "description" = c("From work by Anna Čermáková."),
+    stringsAsFactors = FALSE
+)
+
+b <- as.data.frame(ReadBib("../corpora.bib"), stringsAsFactors = FALSE)
+content <- lapply(setNames(corpora$id, corpora$id), function(x) {
+    tmp <- with(b, b[grepl(paste0('\\{', x, '\\}'), keywords), c('shorttitle', 'title', 'author', 'date')]) 
+    tmp$path <- paste(x, paste0(tmp$shorttitle, '.txt'), sep = '/')
+    row.names(tmp) <- NULL  # else end up in JSON
+    return(tmp)
+})
+
+write_json(list(
+    corpora = corpora,
+    content = content
+), path = '../corpora.json', pretty = TRUE)
+
